f07f8de4f54b24c13c897392f74d5b9a07009186
szierke <sascha.zierke@noocoon.de>
2024-02-24 11:30:38 +0100

    Add load simulation logic, as well store prev simulation in local storage

--- src/components/create-or-load-simulation/CreateOrLoadSimulation.tsx
+++ src/components/create-or-load-simulation/CreateOrLoadSimulation.tsx
@@ -1,11 +1,28 @@
 import { Button, Space, TextInput } from "@mantine/core";
-import { createSimulation } from "../../data/simulation/simulation.api.ts";
+import {
+  createSimulation,
+  fetchSimulationByReference,
+} from "../../data/simulation/simulation.api.ts";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const CreateOrLoadSimulation = () => {
+  const foundPreviousSimulation =
+    localStorage.getItem("simulationReference") || undefined;
+  const [loadSimulationValue, setLoadSimulationValue] = useState(
+    foundPreviousSimulation,
+  );
+
   const navigate = useNavigate();
   const handleCreateSimulation = async () => {
     const simulation = await createSimulation();
+    localStorage.setItem("simulationReference", simulation.reference);
+    navigate(`/${simulation.reference}`);
+  };
+
+  const handleLoadSimulation = async () => {
+    if (!loadSimulationValue) return;
+    const simulation = await fetchSimulationByReference(loadSimulationValue);
     navigate(`/${simulation.reference}`);
   };
 
@@ -15,9 +32,15 @@ export const CreateOrLoadSimulation = () => {
         Create Simulation
       </Button>
       <>
-        <TextInput label={"Simulation Reference"} />
+        <TextInput
+          label={"Simulation Reference"}
+          value={loadSimulationValue}
+          onChange={(event) =>
+            setLoadSimulationValue(event.currentTarget.value)
+          }
+        />
         <Space h={"md"} />
-        <Button bg={"blue.2"} autoContrast>
+        <Button bg={"blue.2"} autoContrast onClick={handleLoadSimulation}>
           Load Simulation
         </Button>
       </>
