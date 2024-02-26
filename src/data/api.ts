01356a917b115f580c591c517f1415cd0e46fcdc
Sascha Zierke <Sascha.zierke@outlook.com>
2024-02-26 23:02:01 +0100

    Revert for cors

--- src/data/api.ts
+++ src/data/api.ts
@@ -5,8 +5,9 @@ export const API_BASE_URL = "https://bs-api.plaesh.de";
 
 export const api = ky.create({
   prefixUrl: API_BASE_URL,
+  credentials: "include",
   headers: {
     "Content-type": "application/json",
   },
-  // mode: "cors",
+  mode: "cors",
 });
