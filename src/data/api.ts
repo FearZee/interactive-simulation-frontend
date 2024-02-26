46a4d89629597b2003941fc12a4e2a694d910e0e
Sascha Zierke <Sascha.zierke@outlook.com>
2024-02-26 21:39:33 +0100

    Remove credentials

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,13 +1,12 @@
 import ky from "ky";
 
-export const API_BASE_URL = "https://bs-api.plaesh.de";
+export const API_BASE_URL = "http://127.0.0.1:8000"; //"https://bs-api.plaesh.de";
 // import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 export const api = ky.create({
   prefixUrl: API_BASE_URL,
-  credentials: "include",
   headers: {
-    "content-type": "application/json",
+    "Content-type": "application/json",
   },
   mode: "cors",
 });
