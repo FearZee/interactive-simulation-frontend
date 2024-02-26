a12f546f1b4846b49c45a7b0d6aded046f50c947
szierke <sascha.zierke@noocoon.de>
2024-02-26 18:48:50 +0100

    Update api to use env

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,6 +1,7 @@
 import ky from "ky";
 
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 export const api = ky.create({
   prefixUrl: API_BASE_URL,
